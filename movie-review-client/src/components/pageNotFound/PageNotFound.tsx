da1d59921b73ed95df0be25daea3112b65c3f6a4
Subha Das <65977621+5ubh4d45@users.noreply.github.com>
2023-02-24 04:20:41 +0530

    added bochhi

--- movie-review-client/src/components/pageNotFound/PageNotFound.tsx
+++ movie-review-client/src/components/pageNotFound/PageNotFound.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
 
 const PageNotFound = () => {
+    const bochhi = 'src/components/pageNotFound/bochhi.png'
     return (
         <div>
-            <h1>Error: 404 |
-                Page not found (T_T)</h1>
+            <Container>
+                <Row>
+                    <Col><h1>Error: 404 | Page not found ╮(╯▽╰)╭</h1></Col>
+                    <Col><div style={{ "background-image": "var(--backdropImg)", "background-size": "auto",
+                        "display": "flex", "background-repeat": "no-repeat", "padding-top": "500px",
+                        "background-position": "center", } as React.CSSProperties}>
+
+                    </div></Col>
+                </Row>
+            </Container>
+
+
         </div>
     );
 };
